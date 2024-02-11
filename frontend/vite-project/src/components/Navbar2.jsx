cf60276971f10cf632202be504a63c9afeacfe9a
sanjaykumardev <duderaj1976@gmail.com>
2024-02-11 23:43:19 +0530

    change

--- frontend/vite-project/src/components/Navbar2.jsx
+++ frontend/vite-project/src/components/Navbar2.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
           ) : (
             <>
               <Link to="/login">
-                <button className="bg-blue-500 shadow-lg shadow-blue-500/50 text-white px-4 py-2 rounded">
+                <button className="bg-blue-500 shadow-lg shadow-blue-500/50 ml-10 text-white px-4 py-2 rounded">
                   Login
                 </button>
               </Link>
